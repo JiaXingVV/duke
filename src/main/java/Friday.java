521ffde234572edd3657df7f7dfca14b2b1ed4e2
JiaXingVV <151317780+JiaXingVV@users.noreply.github.com>
2024-03-05 14:53:53 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -26,7 +26,46 @@ class Task {
         return getStatusIcon() + " " + description;
     }
 }
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
 
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
 public class Friday {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -87,12 +126,40 @@ public class Friday {
                             System.out.println("\n");
                         }
                         continue;
-                    default:
-                        tasks[taskCount] = new Task(command);
+                    case "todo":
+                        tasks[taskCount] = new Todo(taskDescription);
                         taskCount++;
                         System.out.println("Got it. I've added this task:");
                         System.out.println("  " + tasks[taskCount - 1]);
                         System.out.println("Now you have " + taskCount + " tasks in the list.");
+                        System.out.println("\n");
+                        continue;
+                    case "deadline":
+                        String[] deadlineParts = taskDescription.split("/by", 2);
+                        String deadlineDescription = deadlineParts[0].trim();
+                        String by = deadlineParts[1].trim();
+                        tasks[taskCount] = new Deadline(deadlineDescription, by);
+                        taskCount++;
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  " + tasks[taskCount - 1]);
+                        System.out.println("Now you have " + taskCount + " tasks in the list.");
+                        System.out.println("\n");
+                        continue;
+                    case "event":
+                        String[] eventParts = taskDescription.split("/from", 2);
+                        String eventDescription = eventParts[0].trim();
+                        String[] eventTimeParts = eventParts[1].trim().split("/to", 2);
+                        String from = eventTimeParts[0].trim();
+                        String to = eventTimeParts[1].trim();
+                        tasks[taskCount] = new Event(eventDescription, from, to);
+                        taskCount++;
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  " + tasks[taskCount - 1]);
+                        System.out.println("Now you have " + taskCount + " tasks in the list.");
+                        System.out.println("\n");
+                        continue;
+                    default:
+                        System.out.println("Invalid command.");
                         System.out.println("\n");
                         continue;
                 }
