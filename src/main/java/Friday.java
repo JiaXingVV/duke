31f8c3083b0c145c068787c80bde9dbff56f10de
JiaXingVV <151317780+JiaXingVV@users.noreply.github.com>
2024-03-05 11:58:56 +0800

    Level-1.Echo

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
 public class Friday {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("-------------------------------------------------");
         System.out.println("Hello! I'm Friday");
         System.out.println("What can I do for you?");
         System.out.println("-------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("-------------------------------------------------");
+        while (true) {
+            String command = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(command);
+            System.out.println("____________________________________________________________");
+            if (command.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
     }
 }
